3e4450bf9264cbe825d64288d1ee928050b6af1b
Nick <nlweber3@gmail.com>
2018-03-02 23:30:13 -0800

    still missing components

--- bamazon-customer.js
+++ bamazon-customer.js
@@ -17,7 +17,7 @@ con.connect(function(err) {
 	if (err) throw err;
 	console.log("Connected!");
 });
-
+//prompts user for inputs and validates if quantity is a number
 function start() {
 	inquirer
 	.prompt([
@@ -43,3 +43,11 @@ function start() {
 }
 
 start();
+
+//shows products in table in sql database
+connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+    console.table(result);
+});
+//still need to validate quantity in database
